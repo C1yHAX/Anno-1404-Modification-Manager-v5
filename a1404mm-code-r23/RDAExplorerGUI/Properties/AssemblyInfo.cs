ffd6ee2aa3c0cb760e406aa110d769885253d664
C1yHAX <anongfamily@icloud.com>
2026-06-24 01:00:07 +0200

    Update assembly copyright year to 2026
    Set AssemblyCopyright to "Copyright © 2026" across all projects
    (was 2010/2011).

--- a1404mm-code-r23/RDAExplorerGUI/Properties/AssemblyInfo.cs
+++ a1404mm-code-r23/RDAExplorerGUI/Properties/AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System.Windows;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("RDAExplorerGUI")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCopyright("Copyright © 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
